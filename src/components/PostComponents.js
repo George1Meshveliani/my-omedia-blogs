a537195333c7742495c78908189e904f09bfd812
George1Meshveliani <giorgi.meshveliani@gmail.com>
2021-06-02 23:49:49 +0400

    Create post component.

---
+++ src/components/PostComponents.js
@@ -0,0 +1,62 @@
+import { useContext } from "react";
+import { useParams } from 'react-router';
+import { Context } from "../functions/Context";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Alert from 'react-bootstrap/Alert';
+
+
+const PostComponents = () => {
+    const {id} = useParams();
+    const {blogsInfo, blogsLoading, blogsError} = useContext(Context);
+
+    //  const userPostTitle = blogsInfo.map(i => {
+    //      if(i.id === +id) {
+    //          return `${i.title}`;
+    //      }
+    //  });
+    // map is not defined needs to be cleared ... 
+    //  const content = blogsInfo.map(i => {
+    //    if(i.id === +id) {
+    //      return `${i.body}`;
+    //    }
+    //  })
+
+    return (
+        <div>
+            { blogsLoading ? (
+                <div> 
+                    <p> Please wait ... </p>
+                </div>
+            ) : (
+                <div> 
+                    { blogsInfo.map(i => (
+                        <div key={i.id}>
+                            { i.id === +id ? (
+                                <div> 
+                                <h1 className="title">Title: {i.title}</h1>
+                                <Alert variant='light'>
+                                    <img src="https://picsum.photos/200/300"/>
+                                    <h1>content</h1>
+                                    {i.body}
+                                </Alert> 
+                                </div>
+                            ) : (
+                                null
+                            )}
+                        </div>
+                    ))}
+
+                    {/* <h1 className="title">Title: {userPostTitle}</h1>
+                    <Alert variant='light'>
+                        <img src="https://picsum.photos/200/300"/>
+                        <h1>content</h1>
+                        {content}
+                        </Alert>  */}
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default PostComponents;
--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,75 +1,75 @@
 import { useParams } from 'react-router';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
+import PostComponents from '../components/PostComponents';
 import fetchUrl from '../functions/fetchUrl';
 import api from '../functions/api';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Alert from 'react-bootstrap/Alert'
 
-
+// import PostComponents from '../components/PostComponents';
 const Post = () => {
 
-    const { id } = useParams();
+    // const { id } = useParams();
 
-    const postsApi = api('/posts');
-    const userApi  = api('/users');
-    const commentsApi = api('/comments');
+    // const postsApi = api('/posts');
+    // const userApi  = api('/users');
+    // const commentsApi = api('/comments');
 
-    const [ data, setData ] = useState([]);
-    const [ userData, setUserData ] = useState([]);
-    const [ commentsData, setCommentsData ] = useState([]);
+    // const [ data, setData ] = useState([]);
+    // const [ userData, setUserData ] = useState([]);
+    // const [ commentsData, setCommentsData ] = useState([]);
 
-    useEffect(() => {
-        fetchUrl(postsApi, data, setData);
-        fetchUrl(userApi, userData, setUserData);
-        fetchUrl(commentsApi, commentsData, setCommentsData);
-      },[]);
+    // useEffect(() => {
+    //     // fetchUrl(postsApi, data, setData);
+    //     fetchUrl(userApi, userData, setUserData);
+    //     fetchUrl(commentsApi, commentsData, setCommentsData);
+    //   },[]);
 
-     const userPostTitle = data.map(i => {
-         if(i.id === +id) {
-             return `${i.title}`;
-         }
-     });
+    //  const userPostTitle = data.map(i => {
+    //      if(i.id === +id) {
+    //          return `${i.title}`;
+    //      }
+    //  });
 
-     const content = data.map(i => {
-       if(i.id === +id) {
-         return `${i.body}`;
-       }
-     })
+    //  const content = data.map(i => {
+    //    if(i.id === +id) {
+    //      return `${i.body}`;
+    //    }
+    //  })
 
-     const username = userData.map(i => {
-      if(i.id === +id) {
-        return `${i.username}`;
-      }
-    })
+    //  const username = userData.map(i => {
+    //   if(i.id === +id) {
+    //     return `${i.username}`;
+    //   }
+    // })
 
-    const comments = commentsData.map(i => {
-      if(i.postId === +id) {
-        return `${i.body}`;
-      }
-    })
+    // const comments = commentsData.map(i => {
+    //   if(i.postId === +id) {
+    //     return `${i.body}`;
+    //   }
+    // })
     return (
             <div>
-              <h1 className="title">Title: {userPostTitle}</h1>
-              <Link to={`/profile/${id}`} >
+              {/* <Link to={`/profile/${id}`} >
                 <section className="username">
-                <button>
+                  <button>
                     Username: {username}
-                  </button> 
+                  </button>  
                 </section>
-              </Link>
+              </Link> */}
+              {/* <h1 className="title">Title: {userPostTitle}</h1>
               <Alert variant='light'>
               <img src="https://picsum.photos/200/300"/>
               <h1>content</h1>
                 {content}
-              </Alert>
-
-                <Alert variant='info'>
+              </Alert> */}
+              <PostComponents />
+                {/* <Alert variant='info'>
                 <h1>comments</h1>
                 {comments}
-                </Alert>
+                </Alert> */}
             </div>
         )
 }
