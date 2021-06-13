c4a4ed15f74e54029d198d21e7d7d387098ebac7
George1Meshveliani <giorgi.meshveliani@gmail.com>
2021-06-14 01:32:28 +0400

    Create modal example.

--- src/components/CommentsComponents.js
+++ src/components/CommentsComponents.js
@@ -1,6 +1,7 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useParams } from 'react-router';
 import { Context } from "../functions/Context";
+import  Modal from "./Modal";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Alert from 'react-bootstrap/Alert';
@@ -12,6 +13,10 @@ const CommentsComponents = () => {
     const {id} = useParams();
     const {commentsInfo, commentsLoading, commentsError} = useContext(Context);
     
+    const [showModal, setShowModal] = useState(false);
+    const openModal = () => {
+        setShowModal(prev => !prev);
+    }
     return (
         <div>
             { commentsLoading ? (
@@ -37,7 +42,8 @@ const CommentsComponents = () => {
                             )}
                         </div>
                     ))}
-                        <button className="Add-Comment-Button"> Add comment </button>
+                        <button onClick={openModal} className="Add-Comment-Button"> Add comment </button>
+                        <Modal showModal={showModal} setShowModal={setShowModal} /> 
                 </div>
             )}
              {commentsError && <p> Something wrong with API please try again later ... </p>}
---
+++ src/components/Modal.js
@@ -0,0 +1,5 @@
+const Modal = ( {showModal, setShowModal} ) => {
+    return <>{showModal ? <div>Modal</div> : null}</>
+}
+
+export default Modal;
