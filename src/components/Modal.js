563cb2199129b20e3b7c533a1b92c8b6a16d2cc2
George1Meshveliani <giorgi.meshveliani@gmail.com>
2021-06-15 01:37:29 +0400

    Create functional close button.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,7 +4,7 @@ const Modal = ( {showModal, setShowModal} ) => {
     return <>{showModal ? 
     <div>
         <UserComment />
-        <h1>X</h1>
+        <button onClick={() => setShowModal(prev => !prev)}>X</button>
     </div> : null}</>
 }
 
